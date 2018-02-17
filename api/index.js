c8053a8d77a325119ea1df21b544a808f23d00cc
Aurore <aurorem@theodo.fr>
2018-02-17 19:27:36 +0100

    🔎  Add graphiql

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import koaRouter from 'koa-router';
 import koaBody from 'koa-bodyparser';
 import { graphqlKoa } from 'graphql-server-koa';
 import schema from './presentation/schema';
+import graphiql from 'koa-graphiql';
 
 const app = new Koa();
 const router = new koaRouter();
@@ -19,6 +20,12 @@ router.post(
       debug: true,
     };
   })
+);
+router.get(
+  '/graphiql',
+  graphiql(async ctx => ({
+    url: '/api',
+  }))
 );
 
 app.use(koaBody());
